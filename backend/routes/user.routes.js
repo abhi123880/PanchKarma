7f90ee8246cd80a1d92249d25830b97bfec79b95
abhi123880 <nakraniabhishek128@gmail.com>
2025-08-02 23:00:00 +0530

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { updateUser, deleteUser } from "../controllers/user.controller.js";
-
+import (verifyUser) from "../utils/verifyUser.js:;
 const router = express.Router();
 
-router.post("/update/:id", updateUser);
-router.delete("/delete/:id", deleteUser);
+router.post("/update/:id", verifyUser , updateUser);
+router.delete("/delete/:id", verifyUser , deleteUser);
 
 export default router;
